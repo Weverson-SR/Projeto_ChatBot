a5d0b6be858e613c9950076b74b5b13e6d81d445
Weverson <weversonwrs8@gmail.com>
2025-01-27 19:37:34 -0300

    Resolvido o parcialmente o problema do formulário.Falta sumir ele da tela e pular para a conversa se já existe alguém no banco de dados

--- Projeto.py
+++ Projeto.py
@@ -1,31 +1,28 @@
 import google.generativeai as genai
-from dotenv import load_dotenv
-import os
 import streamlit as st
 import sqlite3
+import json
 
 
 class GeminiChat:
 
     def __init__(self):
         """Inicializa a configuração do Gemini."""
-        self.api_key = self._carregar_chave_api()
-        self.model = self._configurar_api(self.api_key)
+        self.sqlite = Sqlite()
+        dados_usuario_form = sqlite.seleciona_usuario()
+        if dados_usuario_form:
+            self.nome_usuario_form = dados_usuario_form['nome']
+            self.chave_api_form = dados_usuario_form['gemini_token']
+        else:
+            raise ValueError("Nenhum dado encontrado no banco de  INIT")
+
+        self.model = self._configurar_api(self.chave_api_form)
         self.chat = self.model.start_chat(history=[])
 
     @staticmethod
-    def _carregar_chave_api():
-        """Carrega a chave da API do arquivo .env."""
-        load_dotenv()
-        chave_api = os.getenv("GENAI_API_KEY")
-        if not chave_api:
-            raise ValueError("A chave da API não foi encontrada. Verifique o arquivo .env.")
-        return chave_api
-
-    @staticmethod
-    def _configurar_api(chave_api):
+    def _configurar_api(chave_api_form):
         """Configura a API do Gemini."""
-        genai.configure(api_key=chave_api)
+        genai.configure(api_key=chave_api_form)
         return genai.GenerativeModel("gemini-1.5-flash")
 
     def gerar_resposta(self, mensagem):
@@ -42,47 +39,13 @@ class GeminiChat:
 
 
 class StreamlitInterface:
-    def __init__(self, gemini_instance):
+    def __init__(self, gemini_instance=None):
         self.gemini = gemini_instance
         self.total_tokens = 0
-
-    def handle_input(self):
-        """Gerencia a entrada do usuário no Streamlit."""
-        # Obtém a entrada do usuário
-        input_usuario = st.session_state.get("user_input", "")
-
-        if input_usuario:  # Verifica se há mensagem do usuário
-            # Adiciona a mensagem do usuário ao histórico
-            st.session_state.chat_history.append({"user": input_usuario})
-
-            try:
-                # Gera a resposta do Gemini
-                resposta_gemini = self.gemini.gerar_resposta(input_usuario)
-
-                # Adiciona a resposta do Gemini ao histórico
-                st.session_state.chat_history.append({"gemini": resposta_gemini})
-
-            except Exception as e:
-                # Em caso de erro, mostra no Streamlit
-                st.session_state.chat_history.append({"gemini": f"Erro ao gerar resposta: {e}"})
-
-            # Calcula o número de tokens da mensagem do usuário
-            user_tokens = len(input_usuario.split())
-
-            # Gera a resposta do Gemini
-            resposta_gemini = self.gemini.gerar_resposta(input_usuario)
-
-            # Calcula o número de tokens da resposta do Gemini
-            gemini_tokens = len(resposta_gemini.split())
-
-            # Soma os tokens da mensagem do usuário e da resposta do Gemini
-            total_tokens = user_tokens + gemini_tokens
-
-            # Soma o total de tokens da conversa
-            self.total_tokens += total_tokens
-
-            # Atualiza o contador
-            st.session_state["total_tokens"] += total_tokens
+        self.nome_usuario_formulario = None
+        self.chave_api_formulario = None
+        self.sqlite = Sqlite()
+        self.json_usuario = JsonUsuario()
 
     def iniciar(self):
         st.sidebar.title("Chat com Gemini")
@@ -104,67 +67,175 @@ class StreamlitInterface:
         if "chat_history" not in st.session_state:
             st.session_state.chat_history = []
 
-        # Markdown para mudar o fundo das mensagens tanto do usuário quanto do gemini
-        st.markdown("""
-            <style>
-                .user-message {
-                    background-color: rgba(102, 102, 102, 0.1);  /* Fundo cinza claro */
-                    padding: 10px;  /* Espaçamento interno */
-                    border-radius: 5px;  /* Borda arredondada */
-                    margin-bottom: 5px;  /* Espaçamento entre as mensagens */
-                    font-size: 14px;  /* Ajuste do tamanho da fonte */
-                }
-                
-                .gemini-message {
-                    padding:10px
-                }
+        if "formulario_preenchido" not in st.session_state:
+            st.session_state.formulario_preenchido = False
 
-            </style>
-        """, unsafe_allow_html=True)
 
-        # Exibição das mensagens com o estilo
-        with st.container():
-            for i, mensagem in enumerate(st.session_state.chat_history):
+        with st.expander("Formulario"):
+            if not self.sqlite.existe_usuario(self.nome_usuario_formulario):
+                with st.form("Formulario"):
+                    st.subheader("Insira seus dados")
+                    self.nome_usuario_formulario = st.text_input("Nome:", placeholder="Digite seu nome")
+                    self.chave_api_formulario = st.text_input("Chave API:", placeholder="Digite sua chave API", type="password")
+                    botao = st.form_submit_button("Salvar")
+
+                    if botao:
+                        # Salva a chave API e o nome do usuário no banco de dados
+                        self.sqlite.insira_usuario(self.nome_usuario_formulario, self.chave_api_formulario)
+                        # Salva os dados do usuário no arquivo JSON
+                        self.json_usuario.nome_usuario = self.nome_usuario_formulario
+                        self.json_usuario.chave_api = self.chave_api_formulario
+                        self.json_usuario.salvar_dados()
+                        st.success("Dados salvos com sucesso!")
+                        st.session_state.formulario_preenchido = True
+
+        if st.session_state.formulario_preenchido:
+            st.write("Agora você pode conversar com o Gemini!")
+            self.handle_input()
+
+            for mensagem in st.session_state.chat_history:
                 if "user" in mensagem:
-                    # Aplica a classe user-message para as mensagens do usuário
-                    st.markdown(f"<p class='user-message'><b>Você:</b> {mensagem['user']}</p>", unsafe_allow_html=True)
+                    st.markdown(f'<div class="user-message">{mensagem["user"]}</div>', unsafe_allow_html=True)
                 elif "gemini" in mensagem:
-                    # Aplica a classe gemini-message para as mensagens do Gemini
-                    st.markdown(f"<p class='gemini-message'><b>Gemini:</b> {mensagem['gemini']}</p>",
-                                unsafe_allow_html=True)
+                    st.markdown(f'<div class="gemini-message">{mensagem["gemini"]}</div>', unsafe_allow_html=True)
 
-        # Caixa de entrada do usuário com callback
-        st.text_input(
-            "Digite sua mensagem aqui",
-            placeholder="Pressione Enter para enviar",
-            key="user_input",
-            on_change=self.handle_input,  # Chama a função ao enviar a mensagem
-        )
+    def handle_input(self):
+        """Gerencia a entrada do usuário no Streamlit."""
+        input_usuario = st.text_input("Digite sua mensagem:" , placeholder="Digite aqui...")
+        if input_usuario:
+            st.session_state.chat_history.append({"user": input_usuario})
+            # Verifica se a instância foi iniciada
+            if self.gemini is None:
+                st.error("Erro: A instãncia do Gemini ainda não foi iniciada.")
+                return
+            try:
+                resposta_gemini = self.gemini.gerar_resposta(input_usuario)
+                st.session_state.chat_history.append({"gemini": resposta_gemini})
+                self.total_tokens += len(input_usuario.split()) + len(resposta_gemini.split())
+                st.session_state["total_tokens"] = self.total_tokens
+            except Exception as e:
+                print(f"Erro ao gerar resposta: {e}")
+            # Ajuste de estilo para manter a caixa no final da página
+        st.markdown("""<style>
+            /* Estilo para manter a caixa de mensagem fixa no rodapé */
+            .stTextInput {
+                position: fixed; /* Fixa a posição no rodapé */
+                bottom: 0; /* Garante que esteja no fundo */
+                max-width: 700px; /* Largura máxima */
+                width: 100%; /* Ajusta à largura total do container */
+                margin: 0 auto; /* Centraliza horizontalmente */
+            }
+        </style>""", unsafe_allow_html=True)
 
-        # Ajuste de estilo para manter a caixa no final da página
-        st.markdown("""
-            <style>
-                /* Estilo para manter a caixa de mensagem fixa no rodapé */
-                .stTextInput {
-                    position: fixed; /* Fixa a posição no rodapé */
-                    bottom: 0; /* Garante que esteja no fundo */
-                    max-width: 700px; /* Largura máxima */
-                    width: 100%; /* Ajusta à largura total do container */
-                    margin: 0 auto; /* Centraliza horizontalmente */
-                }
-            </style>
-        """, unsafe_allow_html=True)
 
+# Markdown para mudar o fundo das mensagens tanto do usuário quanto do gemini
+    st.markdown("""
+        <style>
+            .user-message {
+                background-color: rgba(102, 102, 102, 0.1);  /* Fundo cinza claro */
+                padding: 10px;  /* Espaçamento interno */
+                border-radius: 5px;  /* Borda arredondada */
+                margin-bottom: 5px;  /* Espaçamento entre as mensagens */
+                font-size: 14px;  /* Ajuste do tamanho da fonte */
+            }
+
+            .gemini-message {
+                padding:10px
+            }
+
+        </style>
+    """, unsafe_allow_html=True)
 
 class Sqlite:
     def __init__(self):
-        pass
+        self.conn = sqlite3.connect("gemini.db")
+        self.cursor = self.conn.cursor()
+        self.criar_tabela()
+
+    # Cria uma tabela no banco de dados
+    def criar_tabela(self):
+
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS usuario (
+            nome TEXT NOT NULL,
+            gemini_token TEXT NOT NULL PRIMARY KEY
+            )""")
+        self.conn.commit()
+
+    # Insere dados na tabela
+    def insira_usuario(self, nome_usuario_formulario, chave_api_formulario):
+        self.cursor.execute("""INSERT INTO usuario (nome, gemini_token) VALUES (?, ?) """,
+                            (nome_usuario_formulario, chave_api_formulario))
+        self.conn.commit()
+        self.conn.close()
+
+    # Seleciona o banco de dados
+    def seleciona_usuario(self):
+        self.cursor.execute("SELECT * FROM usuario")
+        dados_usuario_form = self.cursor.fetchone()
+        if dados_usuario_form:
+            return {'nome': dados_usuario_form[0], 'gemini_token': dados_usuario_form[1]}
+        else:
+            return None
+
+    # Verifica se o usuário existe
+    def existe_usuario(self, nome_usuario):
+        cursor = self.conn.cursor()
+        cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS usuario (
+                        id INTEGER PRIMARY KEY,
+                        nome TEXT NOT NULL,
+                        chave_api TEXT NOT NULL
+                    )
+                """)
+        self.conn.commit()
+        # Verifica se o usuário já está cadastrado no banco de dados
+        self.cursor.execute("SELECT * FROM usuario WHERE nome = ?", (nome_usuario,))
+        resultado = self.cursor.fetchone()
+        return resultado is not None
+
+    # Atualiza o usuário
+    def atualiza_usuario(self, nome_usuario_formulario, chave_api_formulario):
+        self.cursor.execute("""UPDATE usuario SET gemini_token = ? WHERE nome = ?""",
+                            (chave_api_formulario, nome_usuario_formulario))
+        self.conn.commit()
+
+
+class JsonUsuario:
+
+    def __init__(self):
+        self.nome_usuario = None
+        self.chave_api = None
+
+    def carregar_dados(self):
+        try:
+            with open('dados_usuario.json', 'r') as arquivo:
+                dados = json.load(arquivo)
+                self.nome_usuario = dados['nome']
+                self.chave_api = dados['chave_api']
+        except FileNotFoundError:
+            pass
+
+    def salvar_dados(self):
+        dados = {'nome': self.nome_usuario, 'chave_api': self.chave_api}
+        with open('dados_usuario.json', 'w') as arquivo:
+            json.dump(dados, arquivo)
 
 
 if __name__ == "__main__":
-    # Inicializa o GeminiChat
-    gemini_chat = GeminiChat()
+    # Verifica se a instância do Gemini já foi armazenada
+    if "gemini_instance" not in st.session_state:
+        sqlite = Sqlite()
+        dados_usuario_form = sqlite.seleciona_usuario()
 
-    # Inicializa a interface do Streamlit
-    interface = StreamlitInterface(gemini_chat)
+        if dados_usuario_form:
+            gemini_chat = GeminiChat()
+        else:
+            gemini_chat = None
+            st.error("Nenhum dado encontrado no banco de dados MAIN.")
+
+        # Armazena a instância no session_state
+        st.session_state["gemini_instance"] = gemini_chat
+
+    # Inicializa a interface com a instância persistida
+    interface = StreamlitInterface(gemini_instance=st.session_state["gemini_instance"])
     interface.iniciar()
